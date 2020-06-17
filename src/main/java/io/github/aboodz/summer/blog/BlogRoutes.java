de1b6f195d42cbe9fd9b30a9fd41088a1fd4f083
aboodz <abd.akilan@hotmail.com>
2020-06-17 20:49:36 +0400

    :fire: removes unused hack
    Removed the reflection hack because it's not good

--- src/main/java/io/github/aboodz/summer/blog/BlogRoutes.java
+++ src/main/java/io/github/aboodz/summer/blog/BlogRoutes.java
@@ -1,9 +1,7 @@
 package io.github.aboodz.summer.blog;
 
 
-import com.google.inject.multibindings.ProvidesIntoSet;
 import io.github.aboodz.summer.server.Routable;
-import io.github.aboodz.summer.server.Router;
 import lombok.extern.log4j.Log4j2;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.server.HttpServerRoutes;
@@ -25,6 +23,7 @@ public class BlogRoutes implements Routable {
     @Override
     public void defineRoutes(HttpServerRoutes routes) {
         routes.get("/blog", (httpServerRequest, httpServerResponse) -> {
+
             Mono<String> blog = blogHandler.getBlog(httpServerRequest);
             return httpServerResponse.sendString(blog);
         });
